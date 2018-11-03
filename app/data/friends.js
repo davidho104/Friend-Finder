92546965cc13c20e88475735fee8698ce5308185
davidho104 <davidho104@gmail.com>
2018-11-03 01:37:58 -0500

    Updates

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+let friendArr = [{
+    "name": "Amy",
+    "photo": "https://cdn.pixabay.com/photo/2016/03/30/21/53/vintage-1291644_640.jpg",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+}, {
+    "name": "Barry",
+    "photo": "https://cdn.pixabay.com/photo/2016/03/30/21/53/vintage-1291644_640.jpg",
+    "scores": [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+
+}, {
+    "name": "Carla",
+    "photo": "https://cdn.pixabay.com/photo/2016/03/30/21/53/vintage-1291644_640.jpg",
+    "scores": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+
+}, {
+    "name": "Daniel",
+    "photo": "https://cdn.pixabay.com/photo/2016/03/30/21/53/vintage-1291644_640.jpg",
+    "scores": [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+
+}, {
+    "name": "Erica",
+    "photo": "https://cdn.pixabay.com/photo/2016/03/30/21/53/vintage-1291644_640.jpg",
+    "scores": [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+}]
+
+module.exports = friendArr;
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var apiRoutes = require("./app/routing/apiRoutes")(app);
+var htmlRoutes = require("./app/routing/htmlRoutes")(app);
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+})
